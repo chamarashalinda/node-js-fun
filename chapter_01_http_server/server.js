b2bc6c9f8629251edb9d43093a35cd058283096b
Chamara Shalinda <chamarashalinda@gmail.com>
2020-11-11 20:40:22 +0530

    On branch chapter_2

--- chapter_01_http_server/server.js
+++
@@ -1,9 +0,0 @@
-import {http} from 'http';
-
-const server = http.createServer((req, res) => {
-  console.log('request made');
-})
-
-server.listen(3000, 'localhost', () => {
-  console.log('Listing to port 3000 in localhost');
-})
